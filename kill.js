6071c74dd0b94ce6febd261ff9d7b25962fbd6a2
kosiso <goodswill94@gmail.com>
2023-04-11 08:56:26 +0100

    My first Js file.

---
+++ kill.js
@@ -0,0 +1,8 @@
+
+function mymy() {
+document.getElementById("isit").style.backgroundColor = "teal";
+document.getElementById('easy').style.backgroundColor = "red";
+document.getElementsByClassName('enter').style.backgroundColor = "black";
+}
+
+
